c17312380293b6258eeced3a76864f6216ffa99b
rodrigobruno <rodrigo@rodrigobruno.com>
2023-02-14 14:28:42 -0300

    Criado modal de cursos, ajustes acessibilidade

---
+++ JS/modal.js
@@ -0,0 +1,69 @@
+const modal = {
+    callOpen: () => {
+        modal.opener = document.activeElement
+        openModal()
+    },
+    callClose: () => {
+        closeModal(modal.opener)
+    },
+    handleOverlayClick: (event) => {
+        if (event.target.className === 'overlay') {
+            closeModal(modal.opener)
+        }
+    }
+}
+
+function openModal() {
+    setVisibile(true)
+    setFocus()
+    setInertBehindModal(true)
+}
+
+function closeModal(opener) {
+    setVisibile(false)
+    setInertBehindModal(false)
+    opener.focus()
+}
+
+function attachEventListener(openButtons, closeButtons, overlay) {
+    openButtons.forEach(b => {
+        b.addEventListener('click', modal.callOpen)
+    });
+    closeButtons.forEach(b => {
+        b.addEventListener('click', modal.callClose)
+    });
+    overlay.addEventListener('click', modal.handleOverlayClick)
+    window.addEventListener('keydown', clallIfEscPress)
+}
+
+function setVisibile(visible) {
+    const display = visible ? 'block' : 'none'
+    document.querySelector('.overlay').style.display = display
+}
+
+function setFocus() {
+    document.querySelectorAll('.modal button, modal input, modal textarea, modal select')[0].focus()
+}
+
+function setInertBehindModal(inert) {
+    const main = document.querySelector('main')
+    const header = document.querySelector('header')
+    const footer = document.querySelector('footer')
+    header.inert = inert
+    main.inert = inert
+    footer.inert = inert
+    header.setAttribute('aria-hidden', inert)
+    main.setAttribute('aria-hidden', inert)
+    footer.setAttribute('aria-hidden', inert)
+}
+
+function clallIfEscPress(event) {
+    if (event.key === 'Escape') {
+        modal.callClose()
+    }
+}
+
+const openButtons = document.querySelectorAll('.open-modal')
+const closeButtons = document.querySelectorAll('.close-modal')
+const overlay = document.querySelector('.overlay')
+attachEventListener(openButtons, closeButtons, overlay)
